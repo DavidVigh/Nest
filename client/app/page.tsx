bf2578c46f11d737dc248ae86b869b64759da271
Vigh David <david.vigh08@gmail.com>
2025-11-04 00:00:30 +0100

    added offer component to landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Header from "./components/header";
+import Offer from "./components/offer";
+import { main } from "framer-motion/client";
 
 
 export default function Home() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <Offer />
+    </main>
   );
 }
