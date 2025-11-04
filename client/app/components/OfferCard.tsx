587df03a420c406ff2c0d65388a5a1fc4e6f00f0
Vigh David <david.vigh08@gmail.com>
2025-11-04 17:12:36 +0100

    CardBg for the card section in the offerCardcomponent

--- client/app/components/OfferCard.tsx
+++ client/app/components/OfferCard.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { CardAnimation } from "../motion/variants";
+import { cardBg } from "../tailwind/variables";
 
 type OfferCardProps = {
   index: number;
@@ -26,7 +27,7 @@ export default function OfferCard({
       }}
       custom={index}
       whileHover="hover"
-      className="flex flex-col items-center justify-center space-y-4 bg-gradient-to-tl from-[#f6e9e3] to-[#faddc8] m-4 p-6 rounded-2xl shadow-lg w-64"
+      className={cardBg + " flex flex-col items-center justify-center space-y-4 m-4 p-6 rounded-2xl shadow-lg w-64"}
     >
       <div className="mx-auto">{icon}</div>
       <h3 className="text-lg text-center font-semibold text-[#3d2d2f]">
