2ba73462bc86ea46328b1c7207d41e0bb0cf27cd
Vigh David <david.vigh08@gmail.com>
2025-11-04 17:11:57 +0100

    created a variables file for preventing repeating code

---
+++ client/app/tailwind/variables.ts
@@ -0,0 +1,7 @@
+export const pageTitle = (textSize:string) => 
+    `text-${textSize} font-semibold text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+
+export const pageQuote = (textSize:string) =>
+    `text-${textSize} mt-1 max-w-xl text-[#3d2d2f]`;
+
+export const cardBg = "bg-gradient-to-tl from-[#f6e9e3] to-[#faddc8]";
