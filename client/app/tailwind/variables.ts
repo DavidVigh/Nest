6fb023aa98aebb834d750074b67faca2260dd2d5
Vigh David <david.vigh08@gmail.com>
2025-11-04 19:58:38 +0100

    added breakpoint switch to pageTitle variable

--- client/app/tailwind/variables.ts
+++ client/app/tailwind/variables.ts
@@ -1,5 +1,15 @@
-export const pageTitle = (textSize:string, breakpoint:string = "") => 
-    `font-semibold ${breakpoint == "" ? "" : breakpoint + ":"}text-${textSize} text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+export const pageTitle = (textSize:string, breakpoint:string = "") => {
+    switch (breakpoint) {
+        case "sm":
+            return `font-semibold sm:text-${textSize} text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+        case "md":
+            return `font-semibold md:text-${textSize} text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+        case "lg":
+            return `font-semibold lg:text-${textSize} text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+        default:
+            return `font-semibold text-${textSize} text-transparent bg-clip-text bg-gradient-to-l from-[#e06e64] to-[#be7343]`;
+    }
+}
 
 export const pageQuote = (textSize:string) =>
     `text-${textSize} mt-1 max-w-xl text-[#3d2d2f]`;
