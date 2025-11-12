3b4bff00b392ce1e0e547ad6ad79cdc50a96ec44
Vigh David <david.vigh08@gmail.com>
2025-11-12 10:26:21 +0100

    refactor types: reorganize ContactCardProps and serviceCard type definitions for improved clarity

--- client/app/types.ts
+++ client/app/types.ts
@@ -8,8 +8,16 @@ export type OfferCardProps = {
 };
 
 export type ContactCardProps = {
-    id: number;
-    icon: React.ReactNode;
-    title: string;
-    text: string;
+  id: number;
+  icon: React.ReactNode;
+  title: string;
+  text: string;
+};
+
+export type serviceCard = {
+  id: number;
+  tag: string;
+  icon: React.ReactNode;
+  title: string;
+  description: string;
 };
