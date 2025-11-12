a70cdbacc9fd8edf06363025b8b0038211a47456
Vigh David <david.vigh08@gmail.com>
2025-11-12 10:27:46 +0100

    add positioning to Footer component: ensure footer is fixed at the bottom of the page

--- client/app/layouts/footer.tsx
+++ client/app/layouts/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full
+        <footer className="bottom-0 w-full
          bg-[#f4e3db] text-center py-4 mt-8">
             <p className="text-sm text-gray-600">
                 &copy; {new Date().getFullYear()} Nest Restaurant. All rights reserved.
